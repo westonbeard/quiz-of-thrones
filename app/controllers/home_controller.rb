df184550194161cadf508e0e68f6b77ebee6c613
Weston Beard <weston.beard@gmail.com>
2015-06-10 17:53:37 -0400

    adding actions to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+  	@question = Question.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+	
+	question_list = [
+		["If you think this has a happy ending, you haven't been paying attention."],
+		["A lion does not concern himself with the opinion of sheep."]
+	]
+
+	question_list.each do |content|
+		Question.create(content: content)
+	end
+
+	answer_list = [
+		["Ramsay Snow", true, 1],
+		["Tywin Lannister", true, 2]
+	]
+
+	answer_list.each do |content, correct, question_id|
+		Answer.create(content: content, correct: correct, question_id: question_id)
+	end

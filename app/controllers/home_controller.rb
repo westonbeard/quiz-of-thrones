8df08ac7a51e80f3e5e3950d8c1819e65c93fcc9
Weston Beard <weston.beard@gmail.com>
2015-06-12 16:38:44 -0400

    took bootstrap import line out of js file, changed home index view to have questions and answers appear

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@question = Question.all
+  	@questions = Question.all
+  	@answers = Answer.all
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
 	has_one :answer
+	# accepts_nested_attributes_for :answer
 end

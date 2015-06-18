a0dd27f36d37faff33dc8ac2ea27a77169ebbc6c
Weston Beard <weston.beard@gmail.com>
2015-06-18 19:50:39 -0400

    changed associations, correctly displaying questions, not answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,3 @@
 class Answer < ActiveRecord::Base
-	has_and_belongs_to_many :questions
+	belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
-	has_and_belongs_to_many :answers
+	has_many :answers
+
+	accepts_nested_attributes_for :answers
 end

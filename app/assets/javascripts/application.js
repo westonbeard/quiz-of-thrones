5a2bc8ddd963f34aef40ce8bcd60eaaebcb5b842
Weston Beard <weston.beard@gmail.com>
2015-06-26 17:21:14 -0400

    added 'quiz' div to questions index. deleted unnecessary code from questions show page and home#form. started writing javascript to render questions for quiz functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,21 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
-//= require bootstrap.min
+
+
+var correct = 0,pos;
+var wrong = 0;
+var percentageCorrect = correct/(questions.length);
+
+var renderQuestions = function (){
+	quiz = getElementbyId("quiz");
+	if(pos >= questions.length){
+		quiz.innerHTML = "<h2>You got "+correct+" (percentage) of "+questions.length+" questions correct.</h2>";
+		_("test_status").innerHTML = "Test Completed";
+		pos = 0;
+		correct = 0;
+		return false;
+		$("#quiz").prepend("Quiz Completed");
+};

57ed0ace8396364394988e2e344532d0e20dc6ef
Weston Beard <weston.beard@gmail.com>
2015-07-02 17:28:23 -0400

    changed from using bootstrap-sass-gem to directly inserting the files to help with precompiling issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,32 +13,32 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require bootstrap.min
 //= require_tree .
 
 
 
 var pos = 0;
 var correct = 0;
-var percentageCorrect = correct/(questions.length);
+var percentageCorrect = correct/2;
 
 $('#submitting_answer').on('submit',function(e){
 
 	var quizStatus = function (){
-		quiz = getElementbyId("quiz");
+		quiz = document.getElementbyId("quiz");
 		//if quiz is finished, display the user's score
-		if(pos >= questions.length){
+		if(pos >= 2){
 			quiz.innerHTML = "<h2>You got "+correct+" "+percentageCorrect+" of "+questions.length+" questions correct.</h2>";
 			$('test_status').innerHTML = "Test Completed";
 			pos = 0;
 			correct = 0;
 			//else, tell the user which question he/she is on
-		}else {
+		}else{
 			$('#test_status').innerHTML = "Question " + pos + " of" + questions.length;
-			}
-		};
+		}
+	};
 
-		
+
 
 
 
@@ -51,16 +51,17 @@ $('#submitting_answer').on('submit',function(e){
 		for(var i=0; i<answers.length; i++){
 			if(answers[i].checked){
 				selectedAnswer = answers[i];
+			}else{window.alert("you must type an answer")
 			};
-
+		};
 		//if the selected answer 
 		if(selectedAnswer.correct == true){
-			correct++
+			correct++;
 		};
 		pos++;
-		quizStatus();
 	}
-};
+})
+
 
 
 

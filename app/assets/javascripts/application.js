35ad1051879d851daa0240bded3cdfcf9b166d78
Weston Beard <weston.beard@gmail.com>
2015-07-06 18:08:36 -0400

    took redundant hrefs out of questions#index. commented out some js to fix the problem with checking answers functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,41 +16,23 @@
 //= require bootstrap.min
 //= require_tree .
 
+$(document).ready(function() {
+    alert("test");
+});
 
 
 var pos = 0;
 var correct = 0;
 var percentageCorrect = correct/2;
 
-$('#submitting_answer').on('submit',function(e){
-
-	var quizStatus = function (){
-		quiz = document.getElementbyId("quiz");
-		//if quiz is finished, display the user's score
-		if(pos >= 2){
-			quiz.innerHTML = "<h2>You got "+correct+" "+percentageCorrect+" of "+questions.length+" questions correct.</h2>";
-			$('test_status').innerHTML = "Test Completed";
-			pos = 0;
-			correct = 0;
-			//else, tell the user which question he/she is on
-		}else{
-			$('#test_status').innerHTML = "Question " + pos + " of" + questions.length;
-		}
-	};
-
-
-
-
-
-
-
-
-	function checkAnswer(){
+// START cleaning up js
+function checkAnswer(){
 		var question = document.getElementById("question");
 		var answers = document.getElementsByName("answers");
 		for(var i=0; i<answers.length; i++){
 			if(answers[i].checked){
 				selectedAnswer = answers[i];
+				console.log("asdfjkl;")
 			}else{window.alert("you must type an answer")
 			};
 		};
@@ -58,9 +40,60 @@ $('#submitting_answer').on('submit',function(e){
 		if(selectedAnswer.correct == true){
 			correct++;
 		};
-		pos++;
+	
 	}
+
+$('[name="submitbtn"]').on('submit',function(e){
+	e.console.log("jkl;jkl;jkl;")
+	var checkingAnswers = function(){
+			quiz.innerHTML = "<h2>You got "+correct+" "+percentageCorrect+" of "+questions.length+" questions correct.</h2>";
+			$('test_status').innerHTML = "Test Completed";
+
+	}
+
 })
+// END cleaning up js
+
+
+// $('#submitting_answer').on('submit',function(e){
+
+// 	var quizStatus = function (){
+// 		quiz = document.getElementbyId("quiz");
+// 		//if quiz is finished, display the user's score
+// 		if(pos >= 2){
+// 			quiz.innerHTML = "<h2>You got "+correct+" "+percentageCorrect+" of "+questions.length+" questions correct.</h2>";
+// 			$('test_status').innerHTML = "Test Completed";
+// 			pos = 0;
+// 			correct = 0;
+// 			//else, tell the user which question he/she is on
+// 		}else{
+// 			$('#test_status').innerHTML = "Question " + pos + " of" + questions.length;
+// 		}
+// 	};
+
+
+
+
+
+
+
+
+// 	function checkAnswer(){
+// 		var question = document.getElementById("question");
+// 		var answers = document.getElementsByName("answers");
+// 		for(var i=0; i<answers.length; i++){
+// 			if(answers[i].checked){
+// 				selectedAnswer = answers[i];
+// 			}else{window.alert("you must type an answer")
+// 			};
+// 		};
+// 		//if the selected answer 
+// 		if(selectedAnswer.correct == true){
+// 			correct++;
+// 		};
+// 		pos++;
+// 	}
+// })
 
 
 

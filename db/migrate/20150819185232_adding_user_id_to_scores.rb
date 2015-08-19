b84ec2e2bd4b396808912ddd6ade3b2df148de8b
Weston Beard <weston.beard@gmail.com>
2015-08-19 15:28:58 -0400

    adding user_id foreign key to Scores

---
+++ db/migrate/20150819185232_adding_user_id_to_scores.rb
@@ -0,0 +1,6 @@
+class AddingUserIdToScores < ActiveRecord::Migration
+  def change
+  	add_column :scores, :user_id, :integer
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814232301) do
+ActiveRecord::Schema.define(version: 20150819185232) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150814232301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal  "correct"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
